a5e7547fd78aaa4b42fa594f58e4fa45ca629c53
Daniel Pfisterer <info@daniel-pfisterer.de>
2015-02-19 12:21:30 +0100

    Revert "task to create typo3 setup.txt"
    This reverts commit a3d3dda9b513294f4a7899652d882a03f79dbb51.

--- gulp-tasks/constants.js
+++ gulp-tasks/constants.js
@@ -13,7 +13,7 @@ module.exports = function (gulp, plugins) {
     '}',
   ].join('\n');
 
-  return gulp.src( srcmap.t3path.Configuration + '/TypoScript/constants.txt' )
+  gulp.src( srcmap.t3path.Configuration + '/TypoScript/constants.txt' )
     .pipe(header(constants, { pkg: pkg } ))
     .pipe(gulp.dest( srcmap.path.dist + '/' + pkg.extkey + '/Configuration/TypoScript'));
   };
