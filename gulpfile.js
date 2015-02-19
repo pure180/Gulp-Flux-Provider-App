85a4e0cdabf63c88dd4b0bf62e3338055b67a71c
Daniel Pfisterer <info@daniel-pfisterer.de>
2015-02-19 12:24:59 +0100

    added streams to build lang files

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 var gulp = require('gulp');
+var del = require('del');
+var runSequence = require('run-sequence');
+var xmlpoke = require('gulp-xmlpoke');
 var plugins = require('gulp-load-plugins')();
+var debug = require('gulp-debug');
 
 var header = require('gulp-header');
 var pkg = require('./src/t3ProviderConf.json');
+var srcmap = require('./gulp-tasks/src.json');
 
 
 // Creates file "ext_tables.php"
@@ -17,9 +22,23 @@ gulp.task('setup', require('./gulp-tasks/setup')(gulp, plugins));
 // Creates file "./Configuration/TypoScript/constants.txt"
 gulp.task('constants', require('./gulp-tasks/constants')(gulp, plugins));
 
+// Creates file "./Configuration/TypoScript/constants.txt"
+gulp.task('createLocallang', require('./gulp-tasks/create_locallang')(gulp, plugins));
+gulp.task('changeLocallang', ['createLocallang'], require('./gulp-tasks/change_locallang')(gulp, plugins));
+gulp.task('locallang', ['changeLocallang']);
+
+gulp.task('clean', function(cb) {
+    del([
+        './dist',
+        './src/tmp'
+      ], cb);
+});
+
+
 gulp.task('create', [
   'ext_tables',
   'ext_emconf',
   'setup',
-  'constants'
+  'constants',
+  'locallang'
 ]);
